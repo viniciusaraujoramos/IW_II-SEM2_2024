ea6008d228564f060f240ddcdbbf515245f5cdc2
Vinicius Araujo Ramos <124802745+viniciusaraujoramos@users.noreply.github.com>
2024-08-08 09:12:09 -0300

    Add files via upload

---
+++ Projeto/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Banner></Banner>
+      <h1>ETEC MCM</h1>
+      <p>Teste de React</p>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ Projeto/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <h2>Desenvolvido por Vcin do pula e bota &reg; 2014</h2>
+        </footer>
+    );
+}
+
+export default Footer;
